f03e64182cd3dccd28c68f2b31c216970c9c5122
AnikaLabiba <anikalabibajahin@gmail.com>
2022-05-22 13:37:27 +0600

    db created and connected

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(cors())
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zrsmd.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("bits-n-bytes").collection("parts");
+    // perform actions on the collection object
+    console.log('db connected');
+    client.close();
+});
+
+
 app.get('/', (req, res) => {
     res.send('Hello from Bits n Bytes.')
 })
