5eee20c6ec770142c597c5e783da79464f791d34
AnikaLabiba <anikalabibajahin@gmail.com>
2022-05-26 19:52:33 +0600

    last changes

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ function verifyJWT(req, res, next) {
     const token = authHeader.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
         if (err) {
-            console.log(err);
             return res.status(403).send({ message: 'Forbidden access' })
         }
         req.decoded = decoded;
