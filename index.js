c34121ee6b7a5f52bb5e059412db9b2b5528c37d
AnikaLabiba <anikalabibajahin@gmail.com>
2022-05-26 20:23:43 +0600

    error solved

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zrsmd.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+console.log(uri)
 //verifing user with jwt
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
